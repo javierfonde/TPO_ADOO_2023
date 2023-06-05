e7c21bddff213b532bb0db1be0a8b17bbd7e3498
NachoLave <110949438+NachoLave@users.noreply.github.com>
2023-06-05 18:05:52 -0300

    Add files via upload

---
+++ src/main/java/modulo_pagos/EstadoDeFactura.java
@@ -0,0 +1,13 @@
+package metodopago;
+
+public class EstadoDeFactura {
+	
+	private Factura Factura;
+	private String Leyenda;
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/modulo_pagos/MercadoPagoApi.java
@@ -0,0 +1,5 @@
+package metodopago;
+
+public class MercadoPagoApi {
+
+}
