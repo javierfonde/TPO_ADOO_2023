b6c02ccd22e82b628f0291bc68e76e86cbbd11bb
gianpeccia <giann.peccia@gmail.com>
2023-06-16 14:48:32 -0300

    1er commit

--- src/main/java/modulo_pagos/Factura.java
+++ src/main/java/modulo_pagos/Factura.java
@@ -8,18 +8,35 @@ import modulo_reserva.Estado;
 
 public class Factura {
 	
-	private long NroDeFactura;
-	private Reserva Reserva;
-	private Cliente Cliente;
-	private Date FechaDeEmision;
-	private Date FechaDeVencimiento;
-	private double Monto;
-	private Estado EstadoFactura;
+	private long nroDeFactura;
+	private Reserva reserva;
+	private Cliente cliente;
+	private Date fechaDeEmision;
+	private Date fechaDeVencimiento;
+	private double monto;
+	private EstadoDeFactura estado;
 	
-	public String Pagar(String respuesta) {
-		return respuesta;
-	}
+	public Factura() {
+        estado = new FacturaPendienteDePago(this);
+    }
+    
+    public void pagar() {
+        System.out.println(estado.getPagar());
+    }
+    
+    public void pagar(EstadoDeFactura nuevoEstado) {
+        estado = nuevoEstado;
+        System.out.println(estado.getPagar());
+    }
+    
+    public String getLeyenda() {
+        return estado.getLeyenda();
+    }
+    
+    public void setEstado(EstadoDeFactura estado) {
+        this.estado = estado;
+    }
+}
 	
 	
 
-}
