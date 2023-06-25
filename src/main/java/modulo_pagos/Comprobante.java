030160a1384304ffef4d6b3f7a0a77b3450cbb9d
NachoLave <110949438+NachoLave@users.noreply.github.com>
2023-06-25 14:34:00 -0300

    Update Comprobante.java

--- src/main/java/modulo_pagos/Comprobante.java
+++ src/main/java/modulo_pagos/Comprobante.java
@@ -5,20 +5,42 @@ package metodopago;
 public class Comprobante {
 	
 	private Factura Factura;
-	private double MontoCobrado;
+	private double MontoACobrar;
 	private double MontoRecibido;
 	private double Vuelto;
 	private long NroDeComprobante;
 	private String MedioDePago;
 	
-	
-	/*public String ToString() {#NO RECUERDO LO QUE DEVUELVE 
-		 
-	}*/
-	
-	public double getMontoRecibido() {
-		return MontoRecibido;
+	public Comprobante(double MontoACobrar, double MontoRecibido, long NroDeComprobante,String MedioDePago ) {
+		double calculo =  MontoRecibido - MontoACobrar;
+		this.MontoACobrar = MontoACobrar;
+		this.MontoRecibido = MontoRecibido;
+		this.Vuelto = this.vuelto(calculo);
+		this.NroDeComprobante = NroDeComprobante;
+		this.MedioDePago = MedioDePago;
+		
 	}
-
-}	
+	
+	public String toString() {
+		return String.format("NroDeComprobante %1$s: Pago %2$s %5$s, de %3$s y se le dio de Vuelto %4$s",NroDeComprobante,MontoRecibido,MontoACobrar,Vuelto,MedioDePago);
+				
+	}
+	public void cambiarMedio(String nuevo) {
+		
+	}
+	
+	private double vuelto (double calculo ) {
+		if (calculo<0) {
+			calculo*=-1;
+		}
+		
+		return calculo;
+	}
+	
+	
+	
+	
+	
+	
+}		
 
